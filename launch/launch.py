8f9ebf9c2da25238a144a435f22fa1ad4ca25aa9
Akashkumar Parmar <112916785+akasparm@users.noreply.github.com>
2023-11-29 17:36:15 -0500

    Launch file upload

---
+++ launch/launch.py
@@ -0,0 +1,45 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+
+def generate_launch_description():
+    bag_record = LaunchConfiguration('bag_record')
+    ROS2_Gazebo_package_dir = get_package_share_directory('ROS2_Gazebo')
+    gazebo_world_path = ROS2_Gazebo_package_dir + '/worlds/ROS2_Gazebo_world.world'
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'bag_record',
+            default_value= 'False'
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                os.path.join(get_package_share_directory(
+                    'turtlebot3_gazebo'), 'launch'), '/turtlebot3_world.launch.py'
+            ])
+        ),
+        # Launch Gazebo with the specified world file
+        # Node(
+        #     package='gazebo_ros',
+        #     executable='gazebo',
+        #     name='gazebo',
+        #     output='screen',
+        #     arguments=['--verbose', '-s', 'libgazebo_ros_init.so', gazebo_world_path]
+        # ),
+        # Launch the ROS2_Gazebo_node
+        Node(
+            package='ROS2_Gazebo',
+            executable='ROS2_Gazebo',
+            output='screen',
+        ),
+        ExecuteProcess(
+            condition=IfCondition(bag_record),
+            cmd=['ros2', 'bag', 'record', '-a', '-x /camera.+'],
+            shell=True
+        )
+    ])
